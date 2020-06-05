068d1eb0ca69f5c11a27d09eb0e3c1c357686a5e
Eljoenai Muninga <eolykab@yahoo.co.za>
2020-06-05 17:06:42 +0200

    Added logic and algorithm

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # fp-bitmaps
+Creating voronoi. This project uses ``JuicyPixels`` library to load and save images. 
+## Requirements
+This project requires ``stack``. You can download and install ``stack`` from https://docs.haskellstack.org/en/stable/README/
+``stack`` is a cross-platform program for developing Haskell projects. It is aimed at Haskellers both new and experienced.
+It features:
+1. Installing GHC automatically, in an isolated location.
+2. Installing packages needed for your project.
+3. Building your project.
+4. Testing your project.
+5. Benchmarking your project.
+## How to run this project
+1. Clone this repository https://github.com/eolykab/fp-bitmaps/
+2. Move into project directory ``cd fp-bitmaps`` and run ``stack setup``
+3. Run ``stack build``
+4. Then run ``stack exec fp-bitmaps-exe``
---
+++ src/App.hs
@@ -0,0 +1,76 @@
+module App
+    ( createVoronoi
+    ) where
+import System.Exit (exitSuccess, exitFailure)
+
+import Data.List
+import Data.Function
+import Codec.Picture
+import System.Random
+
+
+createVoronoi :: IO ()
+createVoronoi = do
+    let srcPath = "images/awesome.png"
+    eitherImg <- readImage srcPath
+    case eitherImg of
+        Left errorMsg -> do
+            putStrLn errorMsg
+            exitFailure
+        Right image -> do
+            putStrLn "Loaded successfully!"
+            let standard = convertRGB8 image;
+            let imgW = imgWidth standard;
+            let imgH = imgHeight standard;
+
+            let randomGen = (mkStdGen 3)
+            let numSeeds = 500
+            let seeds = zip (take numSeeds $ randomRs (0, imgW-1) randomGen) (take numSeeds $ randomRs (0, imgH-1) randomGen)
+            putStrLn "Generating Image..."
+            writePng "images/out.png" (generateImage (renderAlgorithm standard seeds) imgW imgH)
+
+
+    putStrLn "Done"
+    exitSuccess
+
+getPixelR (PixelRGB8 r _ _) = r
+getPixelG (PixelRGB8 _ g _) = g
+getPixelB (PixelRGB8 _ _ b) = b
+imgWidth (Image w h dat) = w
+imgHeight (Image w h dat) = h
+imgData (Image w h dat) = dat
+
+
+offsetsByRadius radius = [(x, y) | x <- [(-radius)..radius], y <- [(-radius)..radius]]
+
+pointsSurrounding radius x y maxX maxY = [(x + fst(a), y + snd(a)) | a <- offsetsByRadius radius, x + fst(a) >= 0, x + fst(a) <= maxX, y + snd(a) >= 0, y + snd(a) <= maxY]
+
+pixelsSurrounding image radius x y maxX maxY = 
+    [pixelAt image (fst point) (snd point) | point <- pointsSurrounding radius x y maxX maxY]
+
+averagePixel :: [PixelRGB8] -> PixelRGB8
+averagePixel pixels = PixelRGB8
+            (fromIntegral r)
+            (fromIntegral g)
+            (fromIntegral b)
+            where
+                r = (sum [getPixelR pixel | pixel <- pixels])
+                g = (sum [getPixelG pixel | pixel <- pixels])
+                b = (sum [getPixelB pixel | pixel <- pixels])
+
+
+
+renderAlgorithm :: Image PixelRGB8 -> [(Int, Int)] -> Int -> Int -> PixelRGB8
+renderAlgorithm original seeds x y = PixelRGB8
+            (fromIntegral $ getPixelR pixel)
+            (fromIntegral $ getPixelG pixel)
+            (fromIntegral $ getPixelB pixel)
+            where 
+                best = closestSeed (x,y) seeds
+                dist = distSquared best (x, y)
+                pixel = averagePixel (pixelsSurrounding original 0 (fst best) (snd best) (imgWidth original) (imgHeight original))
+
+
+distSquared (x1, y1) (x2, y2) = (x1-x2)^2 + (y1-y2)^2
+
+closestSeed p = minimumBy (compare `on` distSquared p)
